fc724a9be1d0d937adddf2565c3c398f7507b621
chanleeip <chanleeip@gmail.com>
2024-01-06 10:57:05 +0530

    bug fix- need to add a database to store the details

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ from typing import Annotated,Union
 from models.model import User
 app=FastAPI()
 
-users_details={}
+users_details=[]
 
 
 @app.get("/")
@@ -33,15 +33,18 @@ def home_page():
     return {"Hello world":"nithinn"}
 
 @app.get("/users/{user_id}")
-async def list_users(
+async def add_users(
     user_id:Annotated[int,Path(...,title="user_id",description="unqiue id for each user")],
     first_name:Annotated[str,Query(max_length=20,title="Enter the first name")],
     last_name:Annotated[str,Query(...,max_length=20,title="Enter the last name")],
     middle_name:Annotated[str,None]=None
     ):
 
-    users_details.update(User(id=user_id,first_name=first_name,middle_name=middle_name,last_name=last_name))
-    return (User(id=user_id,first_name=first_name,middle_name=middle_name,last_name=last_name))
+    users_details.append(User(id=user_id,first_name=first_name,middle_name=middle_name,last_name=last_name).dict())
+    return (users_details)
 
-@app.get("/users/}")
+
+@app.get("/users/")
+async def list_users():
+    return users_details
 
