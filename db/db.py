906cefc512ae6a2db6a73d8140f844024d08c5f6
chanleeip <chanleeip@gmail.com>
2024-01-07 01:47:59 +0530

    created database model

--- db/db.py
+++ db/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime,create_engine,TIMESTAMP
+from sqlalchemy import Column, Integer, String, ForeignKey, Sequence,create_engine,TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import uuid
@@ -14,7 +14,8 @@ session=Session()
 
 class Users(Base):
     __tablename__='users'
-    id=Column(Integer,autoincrement=True,nullable=False)
+    id_sec = Sequence(__tablename__ + "_id_seq")
+    id=Column(Integer, id_sec, server_default=id_sec.next_value(),nullable=False)
     uuid=Column(UUID(as_uuid=True),default=uuid.uuid4,autoincrement=False,primary_key=True)
     user_name=Column(String(30),nullable=False,unique=True)
     first_name=Column(String(30),nullable=False)
