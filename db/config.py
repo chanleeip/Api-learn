1c65a8847e07444a5a81f4ba5b7a38cc16308e70
chanleeip <chanleeip@gmail.com>
2024-01-08 01:03:14 +0530

    added setup for use in render

--- db/config.py
+++ db/config.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-DATABASE_URL="postgresql://wbjhrcfk:Oh9-0QvWApeqi6OWNFirgsWeCBWH3_iE@tiny.db.elephantsql.com/wbjhrcfk"
+DATABASE_URL=os.environ.get('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 Session=sessionmaker(bind=engine)
 session=Session()
